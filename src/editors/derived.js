d8b3db3214691e3d001ea0cefbe278393fbc7bea
Ambroz Bizjak <ambrop7@gmail.com>
2015-01-08 21:09:46 +0100

    Derived editor: Implement support for constant value.

--- src/editors/derived.js
+++ src/editors/derived.js
@@ -1,11 +1,17 @@
 JSONEditor.defaults.editors.derived = JSONEditor.AbstractEditor.extend({
     buildImpl: function() {
         this.always_disabled = true;
-        if (!this.schema.template) {
-            throw "'derived' editor requires the template property to be set.";
+        if (!this.schema.template && !this.schema.hasOwnProperty('constantValue')) {
+            throw "'derived' editor requires the template or constantValue property to be set.";
         }
-        this.template = this.jsoneditor.compileTemplate(this.schema.template, this.template_engine);
-        this.myValue = null;
+        if (this.schema.template) {
+            this.derived_mode = 'template';
+            this.template = this.jsoneditor.compileTemplate(this.schema.template, this.template_engine);
+            this.myValue = null;
+        } else {
+            this.derived_mode = 'constant';
+            this.myValue = this.schema.constantValue;
+        }
     },
     destroy: function() {
         this.myValue = null;
@@ -17,10 +23,12 @@ JSONEditor.defaults.editors.derived = JSONEditor.AbstractEditor.extend({
     },
     setValueImpl: function(val) {
     },
-    onWatchedFieldChange: function() {    
-        var vars = this.getWatchedFieldValues();
-        this.myValue = this.template(vars);
-        this.onChange();
+    onWatchedFieldChange: function() {
+        if (this.derived_mode == 'template') {
+            var vars = this.getWatchedFieldValues();
+            this.myValue = this.template(vars);
+            this.onChange();
+        }
         this._super();
     }
 });
