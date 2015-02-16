ecde6f12e50f7e059d9f045e1dda6324da41a5ac
Ambroz Bizjak <ambrop7@gmail.com>
2015-02-16 23:57:23 +0100

    Register editors before building.

--- src/editor.js
+++ src/editor.js
@@ -48,8 +48,8 @@ JSONEditor.AbstractEditor = Class.extend({
   build: function() {
     var self = this;
     self.withProcessingContext(function() {
-      self.buildImpl();
       self.jsoneditor.registerEditor(self);
+      self.buildImpl();
       self.setupWatchListeners();
       self.updateHeaderText();
       self.onChange();
